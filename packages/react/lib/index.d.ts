a291799304aadd804d5a213e4a3211f7da3dfbbc
SoeRatch <surajkoho@gmail.com>
2024-06-28 18:27:16 +0530

    Remove lib directories from tracking and update .gitignore

--- packages/react/lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-import Button from "./atoms/Button";
-import Color from "./atoms/Color";
-export { Button, Color };
--- packages/react/lib/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Button } from './atoms/Button/Button.js';
-export { default as Color } from './atoms/Color/Color.js';
-//# sourceMappingURL=index.js.map
